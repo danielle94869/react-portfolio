613c18faa0dd24c7611b330a25fbf5ff51f31cb5
Daniel Le <lequangduc512014@gmail.com>
2020-09-22 20:37:49 -0700

    Updates

---
+++ precache-manifest.f3a4e52b636124643191b38209f38239.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57137642018ac39b334f3d089df82b4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "03716deebbc0f0c02f12",
+    "url": "/react-portfolio/static/css/main.33ed33c1.chunk.css"
+  },
+  {
+    "revision": "eba9205eb9ceb1fc5294",
+    "url": "/react-portfolio/static/js/2.0d4fdbfd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.0d4fdbfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03716deebbc0f0c02f12",
+    "url": "/react-portfolio/static/js/main.fa4c175c.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);

4617d8ffbc0178beeabd2f8700eed25d01ad781c
Daniel Le <lequangduc512014@gmail.com>
2020-09-23 20:10:29 -0700

    update

---
+++ src/pages/Portfolio/Portfolio copy.js
@@ -0,0 +1,42 @@
+  
+import React from 'react'
+import {
+  Card, Button, CardImg, CardTitle, CardText, CardDeck,
+  CardSubtitle, CardBody
+} from 'reactstrap';
+
+const Portfolio = (props) => {
+  return (
+    <CardDeck>
+      <Card>
+        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Sensify</CardTitle>
+          <CardSubtitle></CardSubtitle>
+          <CardText>JavaScript</CardText>
+          <a href="https://daniellehillman.github.io/Sensify/"><Button>Check out my project!</Button></a>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>LifeTrack</CardTitle>
+          <CardSubtitle></CardSubtitle>
+          <CardText>JavaScript/Node.js</CardText>
+          <a href=" https://afternoon-stream-93666.herokuapp.com/diary.html"><Button>Check out my project!</Button></a>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Dungeon Sheets</CardTitle>
+          <CardSubtitle></CardSubtitle>
+          <CardText>React.JS/MySql</CardText>
+          <Button>Check out my project</Button>
+        </CardBody>
+      </Card>
+    </CardDeck>
+  );
+};
+
+export default Portfolio;
